464bcd8dda593682df6f6732280df75a580048f1
sunggun1 <readingdeveloper@naver.com>
2023-08-16 14:24:21 +0900

    - jenkins file, orderEdit 수정, WebMvcConfig cors 수정 했지만 안쓰임, Security Config 추가

--- src/main/java/com/example/demo/config/WebMvcConfig.java
+++ src/main/java/com/example/demo/config/WebMvcConfig.java
@@ -3,14 +3,13 @@ package com.example.demo.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 /**
  * 안쓰임.
  */
 //@Configuration
+//@EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Value("${uploadPath}")
@@ -20,5 +19,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
                 .addResourceLocations(uploadPath);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .exposedHeaders("*")
+                .allowCredentials(false).maxAge(3600);
     }
 }
